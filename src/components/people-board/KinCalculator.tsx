fadcd58a9abba6933914ba7f6db7752276816a06
Zequez <zequez@gmail.com>
2022-08-27 17:59:24 -0300

    Actually revert back to Craco; Astro is not what I needed

--- src/components/people-board/KinCalculator.tsx
+++ src/components/people-board/KinCalculator.tsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface KinCalculatorProps {}
 
-const KinCalculator: React.FC<KinCalculatorProps> = () => (
-  <div>
-    <div>Hello thre</div>
-  </div>
-);
+const KinCalculator: React.FC<KinCalculatorProps> = () => {
+  const [name, setName] = useState("Ezequiel");
+  const [dateVal, setDateVal] = useState("1992-12-14");
+
+  return (
+    <div className="w-30 bg-gray-100 shadow-md rounded-md p-4">
+      <input
+        type="text"
+        className="w-full p-2 rounded-md mb-2 text-center"
+        placeholder="Name"
+        value={name}
+        onChange={(ev) => setName(ev.target.value)}
+      />
+      <input
+        type="text"
+        className="w-full p-2 rounded-md text-center"
+        placeholder="YYYY-MM-DD"
+        value={dateVal}
+        onChange={(ev) => setDateVal(ev.target.value)}
+      />
+    </div>
+  );
+};
+
+// interface BeingCardProps = {
+
+// }
+
+// const BeingCard: React.FC<BeingCardProps> = (name, date) => {
+
+// }
 
 export default KinCalculator;
